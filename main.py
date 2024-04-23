f579318ef0fec4428211c01232c3a1ab8eb9256e
cu-sp <down1995@tutanota.com>
2024-04-23 17:37:56 -0400

    final

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Note: For some reason, when I have my airpords connected to my laptop, the code throws an error
 import cv2
 import numpy as np
 import dlib
@@ -14,7 +15,7 @@ while True:
     
     #operations on the frame saved in frame
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray)
+    faces = detector(grey)
 
     #number of faces counter
     i = 0
@@ -26,12 +27,12 @@ while True:
         i +=1
 
         cv2.putText(frame, 'face num' + str(i), (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        cv2.rectangle(frame, (x,y), (x1, y1), (0,255,0), 2)
         print(face ,i)
 
-    cv2.imshow('frame', frame)
+        cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+        break 
 cap.release()
 cv2.destroyAllWindows()
